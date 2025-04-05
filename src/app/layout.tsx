f26f2c604d84643b817f24e3ce4f3513695fe54a
Douglascrc <douglasbuscam@gmail.com>
2025-04-04 23:13:30 -0300

    feat: add site logo and update layout metadata for improved SEO and branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,69 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo_site.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/logo_site.png"
+        />
+        <meta
+          name="description"
+          content="Encontre o imóvel perfeito com o Oceanside: lançamento exclusivo no Recreio dos Bandeirantes, segurança e condições especiais."
+        />
+
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Oceanside</title>
+        <meta
+          name="keywords"
+          content="Oceanside, lançamento, Recreio dos Bandeirantes, imóveis, investimento em imóveis"
+        />
+        <meta name="author" content="Douglas Campos" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="google-site-verification"
+          content="yPQM6TKdAP31dlFHc5Abiq5BDGJ5n4_AsrCEHcZ0dVo"
+        />
+        <meta name="Racca Imovéis" content="Imóveis" />
+        <meta
+          name="Um imóvel de lançamento no Recreio dos bandeirantes"
+          content="Imóvel Ocean Side na Cidade do Rio de Janeiro"
+        />
+
+        <meta
+          name="description"
+          content="Descubra o Oceanside: lançamento exclusivo no Recreio dos Bandeirantes, segurança, qualidade de vida e condições especiais."
+        />
+
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "RealEstateAgent",
+            name: "Racca Imovéis",
+            image: "/logo_site.png",
+            telephone: "+55 (21) XXXX-XXXX",
+            email: "lucindaracca48@gmail.com.br",
+            address: {
+              "@type": "PostalAddress",
+              streetAddress: "Av. das Américas, 12345",
+              addressLocality: "Rio de Janeiro",
+              addressRegion: "RJ",
+              postalCode: "22790-701",
+              addressCountry: "BR",
+            },
+            geo: {
+              "@type": "GeoCoordinates",
+              latitude: "-23.024653",
+              longitude: "-43.469225",
+            },
+            url: "https://www.raccaimoveis.com.br",
+          })}
+        </script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
