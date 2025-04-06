3d9cea8b687e07724d38432a5355fa98a4d195e4
Douglascrc <douglasbuscam@gmail.com>
2025-04-06 13:06:07 -0300

    chore: setup google tagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,10 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K8K6KFDR');`}
+        </Script>
+
         <link
           rel="preconnect"
           href="https://www.youtube.com"
@@ -139,6 +143,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K8K6KFDR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
         <AosInit />
       </body>
